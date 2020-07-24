6cc94136e0e8da8ed0f39604afc1523ed1b721f4
N3CR0 V3N0M <mohammedshabeerkp@gmail.com>
2020-07-24 19:55:05 +0530

    initial commit
    Simple Index page

--- Navigus_Assignment_2/BookIt/urls.py
+++ Navigus_Assignment_2/BookIt/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('features/', views.features, name='features'),
 ]
