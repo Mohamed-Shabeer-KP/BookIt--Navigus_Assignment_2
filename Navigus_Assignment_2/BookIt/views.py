8d0d293fee81934df447d00f243f7fad7ded2474
N3CR0 V3N0M <mohammedshabeerkp@gmail.com>
2020-07-24 19:57:34 +0530

    initial commit
    Basic Index Page

--- Navigus_Assignment_2/BookIt/views.py
+++ Navigus_Assignment_2/BookIt/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 def index(request):
     return render(request,'BookIt/index.html')
+
+def login(request):
+    return render(request,'BookIt/login.html')
+
+def register(request):
+    return render(request,'BookIt/register.html')
+
+def features(request):
+    return render(request,'BookIt/features.html')
